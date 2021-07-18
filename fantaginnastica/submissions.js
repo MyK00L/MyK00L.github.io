7e70ee80abf1da115d7152fbbd5548141854a9ee
myk_00l <michaelchelli00@gmail.com>
2021-07-18 14:43:50 +0200

    m

--- fantaginnastica/submissions.js
+++ fantaginnastica/submissions.js
@@ -1,9 +1,3 @@
 const subs = [
 	// { "name":"nome", "id":"0123456789ab" },
-	{ "name":"TIK TOKyo", "id":"0b10212d303a" },
-	{ "name":"Casella levati", "id":"001123254159" },
-	{ "name":"La dura legge di Simone", "id":"000d2d2e3660" },
-	{ "name":"Fantarstica", "id":"020e142e3160" },
-	{ "name":"Le idi di Maggio", "id":"0c141516324c" },
-	{ "name":"Le Directioner 🤸🏻♂️", "id":"020111274a56" },
 ];
